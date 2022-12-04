39bf1e717fcd58156533d449ab85b2e283e69fe1
RJ Ramirez <rjdtramirez@gmail.com>
2022-12-04 14:05:44 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// Import Solana web3 functinalities
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require("@solana/web3.js");
+const bs58 = require('bs58');
+
+//Get input args
+let arguments = process.argv;
+
+// Get the public and private key from the args
+const publicKey = new PublicKey(arguments[2]).toString();
+const privateKey = Uint8Array.from(bs58.decode(arguments[3]));
+
+// Get the wallet balance from a given private key
+const getWalletBalance = async () => {
+    try {
+        // Connect to the Devnet
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+        // Make a wallet (keypair) from privateKey and get its balance
+        const myWallet = await Keypair.fromSecretKey(privateKey);
+        const walletBalance = await connection.getBalance(
+            new PublicKey(myWallet.publicKey)
+        );
+        console.log(`Wallet balance: ${parseInt(walletBalance) / LAMPORTS_PER_SOL} SOL`);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const airDropSol = async () => {
+    try {
+        // Connect to the Devnet and make a wallet from privateKey
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const myWallet = await Keypair.fromSecretKey(privateKey);
+
+        console.log("Priv: " + privateKey + "\n\nPub:" + myWallet.publicKey);
+        // Request airdrop of 2 SOL to the wallet
+        console.log("Airdropping some SOL to my wallet!");
+        const fromAirDropSignature = await connection.requestAirdrop(
+            new PublicKey(myWallet.publicKey),
+            2 * LAMPORTS_PER_SOL
+        );
+        await connection.confirmTransaction(fromAirDropSignature);
+    } catch (err) {
+        console.log("ERROR!");
+        console.log(err);
+    }
+};
+
+// Show the wallet balance before and after airdropping SOL
+const mainFunction = async () => {
+    await getWalletBalance();
+    await airDropSol();
+    await getWalletBalance();
+}
+
+mainFunction();
